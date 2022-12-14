4cd00be32aee8f523383ab47a520d2e26402a3f8
Anastasia B <nastja.bukhanova@gmail.com>
2022-12-14 14:56:54 +0300

    task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введи цифру дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number <= 5)
+{
+    Console.WriteLine("Нет");
+}
+else if (number > 7)
+{
+    Console.WriteLine("Это не день недели");
+}
+else
+{
+    Console.WriteLine("Да");
+}
